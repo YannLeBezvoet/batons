e3346a0dc2fd1076f8b0b40234f56ca68696c363
YannLeBezvoet <yann.lebezvoet@gmail.com>
2025-09-13 19:59:46 +0200

    just draw in game.go

--- internal/game/game.go
+++ internal/game/game.go
@@ -11,38 +11,17 @@ func Game(screen tcell.Screen) {
 	x := 0
 	y := 0
 	text := "x: " + strconv.Itoa(x) + " y: " + strconv.Itoa(y)
-	quit := make(chan struct{})
 	// Style simple (blanc sur noir)
 	style := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorBlack)
 	screen.SetStyle(style)
-	// Goroutine pour écouter les événements clavier
-	go func() {
-		for {
-			ev := screen.PollEvent()
-			switch ev := ev.(type) {
-			case *tcell.EventKey:
-				if ev.Key() == tcell.KeyEscape {
-					return
-				}
-			case *tcell.EventResize:
-				screen.Sync()
-			}
-		}
-	}()
 	// Boucle principale pour afficher le message
-	for {
-		select {
-		case <-quit:
-			return
-		default:
-			screen.Clear()
-			// Afficher le texte
-			for i, r := range text {
-				screen.SetContent(i, 0, r, nil, style)
-			}
-			// Affiche à l’écran
-			screen.Show()
-			time.Sleep(50 * time.Millisecond)
-		}
+
+	screen.Clear()
+	// Afficher le texte
+	for i, r := range text {
+		screen.SetContent(i, 0, r, nil, style)
 	}
+	// Affiche à l’écran
+	screen.Show()
+	time.Sleep(50 * time.Millisecond)
 }
