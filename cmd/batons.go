60a35bbd14f2f7686189544778143bd9f8f1ad66
YannLeBezvoet <yann.lebezvoet@gmail.com>
2025-09-13 16:41:45 +0200

    move menu in a function

--- cmd/batons.go
+++ cmd/batons.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	menu "batons/internal/menu"
 
 	"github.com/gdamore/tcell"
 )
@@ -17,47 +16,5 @@ func main() {
 		panic(err)
 	}
 	defer screen.Fini()
-
-	quit := make(chan struct{})
-
-	// Goroutine pour écouter les événements clavier
-	go func() {
-		for {
-			ev := screen.PollEvent()
-			switch ev := ev.(type) {
-			case *tcell.EventKey:
-				if ev.Key() == tcell.KeyEscape || ev.Rune() == 'q' {
-					close(quit)
-					return
-				}
-			case *tcell.EventResize:
-				screen.Sync()
-			}
-		}
-	}()
-
-	for {
-		select {
-		case <-quit:
-			return
-		default:
-			// Efface l’écran
-			screen.Clear()
-
-			// Prépare le texte
-			text := fmt.Sprintf("Battons, appuyer sur 'q' ou 'ESC' pour quitter. Heure: %s", time.Now().Format("15:04:05"))
-
-			// Affiche le texte caractère par caractère
-			style := tcell.StyleDefault.Foreground(tcell.ColorGreen)
-			for i, r := range text {
-				screen.SetContent(i+2, 2, r, nil, style)
-			}
-
-			// Affiche à l’écran
-			screen.Show()
-
-			// Attente 50 milisecondes
-			time.Sleep(50 * time.Millisecond)
-		}
-	}
+	menu.Menu(screen)
 }
--- internal/menu/menu.go
+++ internal/menu/menu.go
@@ -1,7 +1,54 @@
 package menu
 
-import "batons/internal/scene"
+import (
+	"fmt"
+	"time"
 
-func menu() {
+	"github.com/gdamore/tcell"
+)
 
+func Menu(screen tcell.Screen) {
+
+	quit := make(chan struct{})
+
+	// Goroutine pour écouter les événements clavier
+	go func() {
+		for {
+			ev := screen.PollEvent()
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyEscape || ev.Rune() == 'q' {
+					close(quit)
+					return
+				}
+			case *tcell.EventResize:
+				screen.Sync()
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-quit:
+			return
+		default:
+			// Efface l’écran
+			screen.Clear()
+
+			// Prépare le texte
+			text := fmt.Sprintf("Batons, appuyer sur 'q' ou 'ESC' pour quitter. Heure: %s", time.Now().Format("15:04:05"))
+
+			// Affiche le texte caractère par caractère
+			style := tcell.StyleDefault.Foreground(tcell.ColorGreen)
+			for i, r := range text {
+				screen.SetContent(i+2, 2, r, nil, style)
+			}
+
+			// Affiche à l’écran
+			screen.Show()
+
+			// Attente 50 milisecondes
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
