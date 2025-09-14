75ce82020606eac3dde1f818e2d4fc97c855e079
YannLeBezvoet <yann.lebezvoet@gmail.com>
2025-09-14 02:18:56 +0200

    gamestruct par référence

--- internal/game/gameKeyHandler.go
+++ internal/game/gameKeyHandler.go
@@ -2,9 +2,21 @@ package game
 
 import "github.com/gdamore/tcell"
 
-func GameKeyHandler(key tcell.Key, gameData GameStruct) int {
+func GameKeyHandler(key tcell.Key, gameData *GameStruct) int {
 	if key == tcell.KeyEscape {
 		return 1
+	}
+	if key == tcell.KeyLeft {
+		gameData.X--
+	}
+	if key == tcell.KeyRight {
+		gameData.X++
+	}
+	if key == tcell.KeyUp {
+		gameData.Y--
+	}
+	if key == tcell.KeyDown {
+		gameData.Y++
 	}
 	return 0
 }
