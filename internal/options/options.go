e359fd2562e955d99afa077b37c1b5d2db1ad2b6
YannLeBezvoet <yann.lebezvoet@gmail.com>
2025-09-14 04:59:26 +0200

    draw option

--- internal/options/options.go
+++ internal/options/options.go
@@ -11,14 +11,31 @@ type OptionsStruct struct {
 	MoveCursorDown  string
 }
 
+var style = tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorBlack)
+var highlightStyle = tcell.StyleDefault.Background(tcell.ColorWhite).Foreground(tcell.ColorBlack)
+
 func Options(screen tcell.Screen, selected int) {
 	mainText := "Options"
 	_ = mainText
 	menu_buttons := []string{"Move Cursor Left", "Move Cursor Right", "Move Cursor Up", "Move Cursor Down", "Back"}
 	_ = menu_buttons
-	style := screen
-	_ = style
-	_ = menu_buttons
-	_ = style
+	width, height := screen.Size()
+	// Calculer la position pour centrer le texte
+	x := (width - len(mainText)) / 2
+	y := height / 2
+	// Affiche le texte caractère par caractère
+	draw(screen, mainText, x, y, false)
+	draw(screen, "=========", x-1, y+1, false)
+	indicationText := "Use arrow keys to navigate and Enter to select"
+	draw(screen, indicationText, (width-len(indicationText))/2, y+2, false)
+}
 
+func draw(screen tcell.Screen, text string, x int, y int, isSelected bool) {
+	usedstyle := style
+	if isSelected {
+		usedstyle = highlightStyle
+	}
+	for i, r := range text {
+		screen.SetContent(x+i, y, r, nil, usedstyle)
+	}
 }
