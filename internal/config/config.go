8896d653ad3ac2c62ebf1520383c25774c0adbe7
YannLeBezvoet <yann.lebezvoet@gmail.com>
2025-09-15 17:24:29 +0200

    init create and load config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,28 @@
 package config
 
 import (
+	"encoding/json"
 	"errors"
 	"os"
 )
+
+func InitConfig(configPath string) (map[string]string, error) {
+	exists, err := CheckStartingConfig(configPath)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		err := CreateDefaultConfig(configPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+	config, err := LoadConfig(configPath)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
 
 func CheckStartingConfig(configPath string) (bool, error) {
 	if _, err := os.Stat("/path/to/whatever"); err == nil {
@@ -15,4 +34,34 @@ func CheckStartingConfig(configPath string) (bool, error) {
 	} else {
 		return false, err
 	}
+}
+
+func CreateDefaultConfig(configPath string) error {
+	file, err := os.Create(configPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	myJson := `{"MoveCursorLeft": "q", "MoveCursorRight": "d", "MoveCursorUp": "w", "MoveCursorDown": "s"}`
+	_, err = file.WriteString(myJson)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadConfig(configPath string) (map[string]string, error) {
+	config := make(map[string]string)
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
 }
