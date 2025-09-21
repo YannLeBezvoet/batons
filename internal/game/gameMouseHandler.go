94e23826a6dd638ae75a0e4c8e48ce2c2f43d0d7
YannLeBezvoet <yann.lebezvoet@gmail.com>
2025-09-21 18:09:39 +0200

    fix du décalage de la caméra

--- internal/game/gameMouseHandler.go
+++ internal/game/gameMouseHandler.go
@@ -11,9 +11,11 @@ import (
 
 func GameMouseHandler(mouseEvent tcell.EventMouse, gameData *GameStruct, config config.ConfigStruct) {
 	const delay = 200 * time.Millisecond // 200ms
+	x, y := mouseEvent.Position()
+	x += gameData.XCamera
+	y += gameData.YCamera
 	switch mouseEvent.Buttons() {
 	case tcell.Button1: // Left click
-		x, y := mouseEvent.Position()
 		if gameData.GameMap[x] == nil {
 			gameData.GameMap[x] = make(map[int]int)
 		}
@@ -21,7 +23,6 @@ func GameMouseHandler(mouseEvent tcell.EventMouse, gameData *GameStruct, config 
 
 	case tcell.Button2: // Right click
 		if utils.CheckDelay(gameData.LastRightClickTime, delay) {
-			x, y := mouseEvent.Position()
 			newStickman := stickman.NewStickman(x, y)
 			gameData.StickManSlice = append(gameData.StickManSlice, newStickman)
 			gameData.LastRightClickTime = time.Now()
